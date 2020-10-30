7be3625c70c5e192a829b22d4a48b7f623f28072
Taylor Jones <taylor@taylorwjones.com>
2020-10-30 11:44:06 -0500

    Working on minimax AI

--- app.js
+++ app.js
@@ -79,14 +79,30 @@ const Game = (() => {
     }
 
     const winningPlayer = function() {
+        let winner = null;
         let winLocation = checkForWinner();
         let winMarker = Gameboard.gameBoard[winLocation[0]];
         if (winMarker === "X") {
-            return player1;
+            winner = "X";
+            return {score: -10}
         } else if (winMarker === "O") {
-            return player2;
+            winner = "O"
+            return {score: +10}
         }
-        removeEvents();
+        let openSpots = 0;
+        for (let i = 0; i < Gameboard.gameBoard.length; i++) {
+            if (Gameboard.gameBoard[i] === "") {
+                openSpots++;
+            }
+        }
+        if (winner === null && openSpots === 0) {
+            winner = "tie";
+            return {score: 0}
+        } else {
+           return winner;
+        }
+
+        return winner;
     }
     
     let turn = 1;
@@ -94,41 +110,59 @@ const Game = (() => {
         return (turn % 2 === 0) ? player2 : player1;
     }
     // Player move
-    const move = (e) => {
-        if (!gameOver){}
+    // const move = (e) => {
+    //     if (!gameOver){}
+    //     let activePlayer = whosTurn();
+    //     if (activePlayer === player1) {
+    //         if (e.target.innerText === "") {
+    //             Gameboard.updateBoard(e.target.getAttribute("data-value"), player1.symbol)
+    //             console.log(e.target.value);
+    //             console.log(player1.symbol);
+    //             Gameboard.boardRender();
+    //             checkForWinner();
+    //             if (winningCombo.length !== 0) {
+    //                 console.log(winningPlayer().name);
+    //                 Display.updateWinnerDiv(winningPlayer());
+    //             }
+    //             turn++;
+    //         }
+    //     } else if (activePlayer === player2) {
+    //         bestMove();
+    //     }
+    // }
+
+    const moves = (e) => {
         let activePlayer = whosTurn();
-        if (e.target.innerText === "") {
-            Gameboard.updateBoard(e.target.getAttribute("data-value"), activePlayer.symbol)
-            console.log(e.target.value);
-            console.log(activePlayer.symbol);
-            Gameboard.boardRender();
-            checkForWinner();
-            if (winningCombo.length !== 0) {
-                console.log(winningPlayer().name);
-                Display.updateWinnerDiv(winningPlayer());
-            } else {
+        if (activePlayer === player1) {
+            if (e.target.innerText === "") {
+                Gameboard.updateBoard(e.target.getAttribute("data-value"), activePlayer.symbol)
+                console.log(player1.symbol);
+                Gameboard.boardRender();
                 turn++;
             }
-        } else if (e.target.innerText !== "") {
-            alert("Sorry, you cannot play there");
+        } else if (activePlayer === player2) {
+            bestMove()
+            Gameboard.boardRender();
+            turn++;
         }
     }
+    
 
     const removeEvents = () => {
         Gameboard.boardTiles.forEach((tile) => {
-            tile.removeEventListener('click', move);
+            tile.removeEventListener('click', moves);
         });
     }
     function addEvents() {
         Gameboard.boardTiles.forEach((tile) => {
-            tile.addEventListener('click', move);
+            tile.addEventListener('click', moves);
         })
     }
 
     const updateWinner = () => {
-        if (checkForWinner === player1) {
+        if (winningPlayer() === "X") {
             winner = player1;
-        } else if (checkForWinner === player2) {
+        } else if (winningPlayer() === "O") {
             winner = player2;
         }
     }
@@ -146,6 +180,65 @@ const Game = (() => {
     }
 
     addEvents();
+
+    function bestMove() {
+        let bestScore = -1000;
+        let activeplayer = whosTurn();
+        let move;
+        for (let i = 0; i < 9; i++) {
+            if (Gameboard.boardTiles[i].innerText === "") {
+                Gameboard.updateBoard(i, player1.symbol);
+                let score = minimax(Gameboard.boardTiles, 0, false);
+                Gameboard.updateBoard(i, "");
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = i
+                    // console.log(move);
+                }
+            }
+        }
+        Gameboard.updateBoard(move, player2.symbol);
+        Gameboard.boardRender();
+        return move;
+    }
+    let scores = {
+        O: 100,
+        x: -100,
+        tie: 0
+    }
+
+    console.log(winningPlayer());
+
+    function minimax(board, depth, isMaximizing) {
+        let result = winningPlayer();
+        if (result !== null) {
+            return scores[result];
+        }
+        if (isMaximizing) {
+            let bestScore = -1000;
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === "") {
+                    Gameboard.updateBoard(i, player2.symbol);
+                    let score = minimax(Gameboard.gameBoard, depth + 1, false);
+                    bestScore = Math.max(score, bestScore);
+                    Gameboard.updateBoard(i, "");
+                }
+            }
+            return bestScore;
+        } else {
+            let bestScore = 1000;
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === "") {
+                    Gameboard.updateBoard(i, player1.symbol);
+                    let score = minimax(Gameboard.gameBoard, depth + 1, true);
+                    bestScore = Math.min(score, bestScore);
+                    Gameboard.updateBoard(i, "");
+                }
+            }
+            return bestScore;
+        }
+    }
+    console.log(minimax(Gameboard.gameBoard, 0, false));
 
     return {removeEvents, winningPlayer, updateWinner, resetGame}
 })();
@@ -168,3 +261,4 @@ const Display = (() => {
 
     return {updateWinnerDiv}
 })();
+
